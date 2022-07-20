d8af98eb4077b87b6ed164f3b6d65cfd4c0c1731
Aryailia <Aryailia@users.noreply.github.com>
2022-07-20 23:17:49 +0400

    typo

--- yt-archive/archive.sh
+++ yt-archive/archive.sh
@@ -256,7 +256,7 @@ my_make() {
         printf %s\\n "${dump}" | jq --slurp 'sort_by(.title)'
 
     ;; list-as-archive)
-      my_make list_stems "${2}" | sed "s/^/youtube /"
+      my_make list-stems "${2}" | sed "s/^/youtube /"
 
     ;; list-stems) # <directory>
       [ -d "${2}" ] || die FATAL 1 "Arg two '${2}' must be a directory"
