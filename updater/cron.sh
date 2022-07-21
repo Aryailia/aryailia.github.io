e3e3608ad334721e3d65a451a7baa2ff766e543f
Aryailia <Aryailia@users.noreply.github.com>
2022-07-21 22:56:11 +0400

    Actual cron script

--- updater/cron.sh
+++ updater/cron.sh
@@ -3,10 +3,10 @@
 WD="$( dirname "$0"; printf a )"; WD="${WD%?a}"
 cd "${WD}" || { printf "Could not cd to directory of '%s'" "$0" >&2; exit 1; }
 
-../ablc-data/make.sh download-rss
-../ablc-data/make.sh archive-meta-and-subs
-../ablc-data/make.sh download-missing-subs
-../ablc-data/make.sh archive-missing-subs
-../ablc-data/make.sh compile
-./trigger-update.sh
+[ -d "../ablc-data" ] || { printf %s\\n "'ablc-data' does not exist"; exit 1; }
+
+../ablc-data/make.sh update-by-rss
+if ../ablc-data/make.sh save-update; then
+  ./trigger-update.sh
+fi
 
